613290b344b929f19bf34806b70381fb734e9559
Ricardo Salles <salles.ricardo@gmail.com.br>
2024-04-15 16:51:32 -0300

    feat: Request auth token to movie CRUD

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -6,35 +6,42 @@ import {
     Patch,
     Param,
     Delete,
+    UseGuards,
 } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('movies')
 export class MoviesController {
     constructor(private readonly moviesService: MoviesService) {}
 
+    @UseGuards(AuthGuard)
     @Post()
     create(@Body() createMovieDto: CreateMovieDto) {
         return this.moviesService.create(createMovieDto);
     }
 
+    @UseGuards(AuthGuard)
     @Get()
     findAll() {
         return this.moviesService.findAll();
     }
 
+    @UseGuards(AuthGuard)
     @Get(':id')
     findOne(@Param('id') id: string) {
         return this.moviesService.findOne(+id);
     }
 
+    @UseGuards(AuthGuard)
     @Patch(':id')
     update(@Param('id') id: string, @Body() updateMovieDto: UpdateMovieDto) {
         return this.moviesService.update(+id, updateMovieDto);
     }
 
+    @UseGuards(AuthGuard)
     @Delete(':id')
     remove(@Param('id') id: string) {
         return this.moviesService.remove(+id);
